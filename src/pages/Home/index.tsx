83a16de9aafaecd3a436cf8f6f91ac0cb0c25423
just like before <agangdi@gmail.com>
2021-06-04 18:08:02 +0800

    t has one bug

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -83,16 +83,16 @@ export default function Home() {
                 <img height="160px" style={{ marginTop: 0 }} src={smallLogo} alt="Home" />
               </RowBetween>
               <RowBetween style={{ justifyContent:"center" }}>
-                <TYPE.darkGray fontSize={14} >{t('The Art of TEAsWAP ( "TSA" )  is now your home for incredible , innovative NFT Art. Together, we invite you to dive into this new  era of digital enlightenment.')}</TYPE.darkGray>
+                <TYPE.darkGray fontSize={14} >The Art of TEAsWAP ( "TSA" )  is now your home for incredible , innovative NFT Art. Together, we invite you to dive into this new  era of digital enlightenment.</TYPE.darkGray>
               </RowBetween>
               <RowBetween style={{ justifyContent:"center" }}>
-                <TYPE.darkGray fontSize={14} >{t('TSA is a new cross -chain marketplace of digital NFT Art built on Binance Smart Chain  and Ethereum network, powered by the native governance token TSA and DeFi solutions.')}</TYPE.darkGray>
+                <TYPE.darkGray fontSize={14} >TSA is a new cross -chain marketplace of digital NFT Art built on Binance Smart Chain  and Ethereum network, powered by the native governance token TSA and DeFi solutions.</TYPE.darkGray>
               </RowBetween>
               <RowBetween style={{ justifyContent:"center" }}>
-                <TYPE.darkGray fontSize={14} >{t('TSA brings together creators, collectors, curators, influencers, brokers, wallets, auctioneers around the world to the NFT digital space. It currently has 3 spaces: NFT collections, NFT Broadway, and NFT Incubator.')}</TYPE.darkGray>
+                <TYPE.darkGray fontSize={14} >TSA brings together creators, collectors, curators, influencers, brokers, wallets, auctioneers around the world to the NFT digital space. It currently has 3 spaces: NFT collections, NFT Broadway, and NFT Incubator.</TYPE.darkGray>
               </RowBetween>
               <RowBetween style={{ justifyContent:"center" }}>
-                <TYPE.darkGray fontSize={14} >{t('TSA also has been created to give TSA community the power to influence decisions and incentivize active participation, like RARI from Rarible.')}</TYPE.darkGray>
+                <TYPE.darkGray fontSize={14} >TSA also has been created to give TSA community the power to influence decisions and incentivize active participation, like RARI from Rarible.</TYPE.darkGray>
               </RowBetween>
             </AutoColumn>
           </CardSection>
