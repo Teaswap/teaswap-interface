bb0bab98c3dfb2c2f8d3b83d000a4e4b0f9f3e6f
Icashrewards <blockchainimpact@gmail.com>
2021-07-14 13:39:10 -0700

    user-circle icon get bigger to 35px

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -10,9 +10,9 @@ const Icon = styled.i`
   background: url(${process.env.PUBLIC_URL}/svg/${(props) => props.kind}.svg)
     center no-repeat;
   margin: 8px ${(props) => (props.$margin === 0 ? 0 : 11)}px;
-  background-size: 24px;
-  height : 24px;
-  width : 24px;
+  background-size: 35px;
+  height : 35px;
+  width : 35px;
 
 `;
 
