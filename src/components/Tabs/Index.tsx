31aa57b8197debdda9ee005189afc151baf58ec8
Icashrewards <blockchainimpact@gmail.com>
2021-07-13 17:09:03 -0700

    change font size

--- src/components/Tabs/Index.tsx
+++ src/components/Tabs/Index.tsx
@@ -6,7 +6,7 @@ import Typography from '@material-ui/core/Typography';
 
 const AntTabs = withStyles({
   root: {
-    borderBottom: '1px solid #e8e8e8',
+    borderBottom: '1px solid #e8e8e8',fontsize:'14px',
   },
   indicator: {
     backgroundColor: '#DCDCDC',
