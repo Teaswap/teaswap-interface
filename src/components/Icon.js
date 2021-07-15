40a7c3d5a1d65617b5b1b8bf1c0ef5faad90446d
Icashrewards <blockchainimpact@gmail.com>
2021-07-15 16:00:39 -0700

    change shopingicon margin

--- src/components/Icon.js
+++ src/components/Icon.js
@@ -9,7 +9,7 @@ const IconContainer = styled.div`
 const Icon = styled.i`
   background: url(${process.env.PUBLIC_URL}/svg/${(props) => props.kind}.svg)
   center no-repeat;
-  margin: 8px ${(props) => (props.$margin === 0 ? 0 : 11)}px;
+  margin: 8px ${(props) => (props.$margin === 0 ? 0 : 8)}px;
   background-size: 30px;
   height : 30px;
   width : 30px;
