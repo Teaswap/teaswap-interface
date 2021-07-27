a1649a39d35cdf3e0c6445a3826b1b9523487531
Icashrewards <blockchainimpact@gmail.com>
2021-07-27 14:46:43 -0700

    adding style to menue

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -58,7 +58,7 @@ const StyledMenu = styled.div`
 `
 
 const MenuFlyout = styled.span`
-  min-width: 9rem;
+  min-width: 8rem;
   box-shadow: 0px 0px 1px rgba(0, 0, 0, 0.01), 0px 4px 8px rgba(0, 0, 0, 0.04), 0px 16px 24px rgba(0, 0, 0, 0.04),
     0px 24px 32px rgba(0, 0, 0, 0.01);
   border-radius: 0px;
