d2fc9f13996471345673d44843112b00386eb198
Icashrewards <blockchainimpact@gmail.com>
2021-07-29 16:02:33 -0700

    add style

--- src/components/cartSystem/CartItem.js
+++ src/components/cartSystem/CartItem.js
@@ -1,11 +1,24 @@
 import styled from "styled-components";
 import React from "react";
 import { IconComponent } from "../../components";
-import { COLOR, FONT } from "../../constants/style";
+import {
+  COLOR,
+  FONT,
+  MEDIA_QUERY_MD,
+  DISTANCE,
+} from "../../../constants/style";
 
 import ItemDetail from "./ItemDetail";
 import useCart from "../../hooks/cartHooks/useCart";
 import {useTranslation} from "react-i18next";
+
+const TableContainer = styled.tr``;
+const Tableth = styled.th`
+  font-size: ${FONT.xsm};
+  color: ${COLOR.black};
+  border-bottom: solid 1px ${COLOR.cccccc};
+  padding: ${DISTANCE.sm};
+`;
 
 const Container = styled.div`
   margin: 0 auto;
@@ -17,9 +30,8 @@ const Container = styled.div`
 `;
 
 const Top = styled.div`
-  background: #DCDCDC;
-  border: 2px solid #DCDCDC;
-  padding: 15px;
+  background: #EDEEF2;
+  border: 1px solid #EDEEF2;
   border-radius: 0px 0px 0 0;
   display: flex;
   justify-content: space-between;
@@ -159,7 +171,7 @@ export default function CartItem({ cart }) {
       <Table>
         <thead>
           <tr style={{height: "60px"}}>
-            <th>{t("Product")}</th>
+            <th>{t("Item")}</th>
             <th>{t("Name")}</th>
             <th>{t("Price")}</th>
             <th>{t("Bidprice")}</th>
