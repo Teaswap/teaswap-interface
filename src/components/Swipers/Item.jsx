5cccc22e84bb5ee6dea4ab8143d5ca93100eda80
Icashrewards <blockchainimpact@gmail.com>
2021-09-28 13:09:46 -0700

    add style

--- src/components/Swipers/Item.jsx
+++ src/components/Swipers/Item.jsx
@@ -8,11 +8,18 @@ const Item = ({img}) => {
     	<img className="nft-swiper-img" src={img}/>
 			<span style={{
 				position: 'absolute',
-				bottom: 20,
-				left: 20,
-				padding: '5px 20px',
+				top: 10,
+				left: 10,				
+				padding: '5px 10px',
 				backgroundColor: "#ffffff"
-			}}>0.18 ETH</span>
+			}}>🔥 PRESALE 10/01</span>
+			<span style={{
+				position: 'absolute',
+				bottom: 5,
+				right: -95,
+				padding: '1px 4px',
+				backgroundColor: "#ffffff"
+			}}>sale price 0.18 ETH</span>
 		</div>
 	)
 };
