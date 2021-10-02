b8cf4cfb8267e92fd5410b7eb3c349d5a81bfc03
just like before <agangdi@gmail.com>
2021-10-02 23:20:26 +0800

    Invalid email

--- src/hooks/userHooks/useVendorForm.js
+++ src/hooks/userHooks/useVendorForm.js
@@ -21,8 +21,8 @@ export default function useVendorForm() {
   const [emailError, setEmailError] = useState('');
 
   const handleSubmit = (setSuccessMode, isAdminStatus, goPost=false) => {
-    // const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    // if(!re.test(String(email).toLowerCase())) return setEmailError('Email format is wrong')
+    const re = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z]{2,4})$/;
+    if(!re.test(String(email).toLowerCase())) return setEmailError('Invalid email')
     if (nickname && !nickname.trim()) return setNicknameError('姓名格式錯誤');
     if (!email) return setEmailError('*This field is required.');
     setNicknameError('');
