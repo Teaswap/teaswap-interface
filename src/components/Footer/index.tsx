92e0e781e04b60f5098bf1dd72b476a25edb5925
Icashrewards <blockchainimpact@gmail.com>
2021-10-12 15:48:20 -0700

    add style

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -12,49 +12,40 @@ const Footer: React.FC = () => {
     <StyledFooter>
       <StyledFooterInner>
         <StyledFooterItemLeft style={{ marginBottom: 20, width: '150px', textAlign: 'left' }}>
-          <img height="40px" style={{ marginTop: -8 }} width="" src={Logo} alt="logo" />
+          <img height="40px" style={{ marginTop: -8 }} width="" src={Logo} alt="logo"   />
           <StyledFooterSmallItemTitle>The One Hub for NFT Creators and Influencers.</StyledFooterSmallItemTitle>
-          <StyledFooterItemTitle>© 2021 TSANFT All rights reserved.</StyledFooterItemTitle>
-            
-          {/* <StyledLink  target="_blank" href={'mailto:support@teaswap.art'}>support@teaswap.art</StyledLink> */}
         </StyledFooterItemLeft>
         <StyledFooterItem>
           <Nav />
           <NavSocial />
-         
         </StyledFooterItem>
-       
       </StyledFooterInner> 
-       {/* <StyledFooterItemDiv  >
-          <StyledFooterItemTitle>
-            © 2021 TSANFT All rights reserved.
-            </StyledFooterItemTitle>
-        </StyledFooterItemDiv> */}
+      <StyledFooterItemDiv>
+                <div >
+                    <div >
+                        <span >© 2021 TSANFT All rights reserved.</span>
+                    </div>
+                </div>
+           
+      </StyledFooterItemDiv>
     </StyledFooter>
   )
 }
-// const StyledFooterItemDiv = styled.div`
-// width:100%;  
-// display: inline-block;
-//   vertical-align: down;
-// // display: 0 0 100%;
-// //   max-width: 100%;
-// //   flex-direction: column;
-// //   align-items: flex-start;
-// //   justify-content: center;
-// //   margin: 10px 0;
-// //   position: relative;
-// //     width: 100%;
-// //     min-height: 1px;
-// //     padding-right: 15px;
-// //     padding-left: 15px;
-// `
+const StyledFooterItemDiv = styled.div`
+width:100%;  
+text-align: center!important;
+font-size: 10px;
+font-weight: 500;
+color: #FFFFFF;
+`
 const StyledFooter = styled.footer`
   align-items: flex-start;
   display: flex;
+  flex-direction: column;
   justify-content: center;
   width: 100%;
   background: #303030;
+  padding :45px;
   ${({ theme }) => theme.mediaWidth.upToExtraSmall`
     padding-bottom: 40px;
   `} 
@@ -63,8 +54,8 @@ const StyledFooterInner = styled.div`
   align-items: flex-start;
   display: flex;
   justify-content: space-between;
-  width: 90%;
-  padding: 60px 15px 54px;
+  width: 98%;
+  padding: 50px 50px 54px;
   box-sizing: border-box;
   ${MEDIA_QUERY.sm} {
     width: 98%;
@@ -89,23 +80,23 @@ const StyledFooterItem = styled.section`
   margin: 10px 0;
 `
 const StyledFooterSmallItemTitle = styled.span`
-  font-size: 10px;
+  font-size: 13px;
   font-weight: 500;
   color: #FFFFFF;
   line-height: 24px;
-  min-width: 220px;
-  margin-top: 22px;
+  min-width: 285px;
+  margin-top: 0px;
   text-align:center;
 `
 
-const StyledFooterItemTitle = styled.span`
-  font-size: 10px;
-  font-weight: 500;
-  color: #FFFFFF;
-  line-height: 24px;
-  min-width: 170px;
-  margin-top: 22px;
-`
+// const StyledFooterItemTitle = styled.span`
+//   font-size: 10px;
+//   font-weight: 500;
+//   color: #FFFFFF;
+//   line-height: 24px;
+//   min-width: 170px;
+//   margin-top: 22px;
+// `
 // const StyledLink = styled.a`
 //   color: ${({ theme }) => theme.bg2};
 //   text-decoration: none;
