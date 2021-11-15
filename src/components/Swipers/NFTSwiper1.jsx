2813baa528f8d4822772cc3ed9f67818a5b2091a
Icashrewards <blockchainimpact@gmail.com>
2021-11-15 10:43:41 -0800

    add new style

--- src/components/Swipers/NFTSwiper1.jsx
+++ src/components/Swipers/NFTSwiper1.jsx
@@ -18,58 +18,69 @@ export default function App() {
   let slides = [   
     {
       key: 1,
-      content: <Item img={process.env.PUBLIC_URL  + '/nft/products/07Punk Penguin.png'}  amount= 'PRESALE 10/01'/>,
-      link: 'https://www.teaswap.art/nft/products/vendor/265054',
+      content: <Item img={process.env.PUBLIC_URL  + '/nft/products/11Sushi Penguin.png'}  amount= 'PRESALE 11/18'/>,
+      link: 'https://treasureland.market/collection/0x8db099f2d73016cfbdac978bc21e5470e7fbaaa0?chain_id=56',
       
     },
-    // {
-    //   key: 2,
-    //   content: <Item img= {process.env.PUBLIC_URL  + '/nft/products/lots.gif'} amount= 'PRESALE 10/01' />,
-    //   link: 'https://www.teaswap.art/nft/products/vendor/264202',
-      
-    // },
-    // {
-    //   key: 2,
-    //   content: <Item img="https://i.imgur.com/miBU6TC.jpg" amount= 'PRESALE 10/01'/>,
-    //   link:  process.env.PUBLIC_URL + '/nft/products/vendor/264197',
-    // https://i.imgur.com/WoHWbBf.jpg
-    // },   
-    // {
-    //   key: 2,
-    //   content: <Item img= {process.env.PUBLIC_URL  + '/nft/products/Wizard penguin.mp4'} amount= 'PRESALE 10/01'/>,
-    //   link:  process.env.PUBLIC_URL + '/nft/products/vendor/264197',
-      
-    // },   "https://i.imgur.com/WoHWbBf.jpg"
-  
     {
       key: 2,
-      content: <Item img= {process.env.PUBLIC_URL  + '/nft/products/NMsquare.png'} amount= 'PRESALE 10/06' />,
-      link: 'https://www.teaswap.art/nft/products/vendor/264197',     
+      content: <Item img={process.env.PUBLIC_URL  + '/nft/products/12Chinese food Penguin.png'}  amount= 'PRESALE 11/18'/>,
+      link: 'https://treasureland.market/collection/0x8db099f2d73016cfbdac978bc21e5470e7fbaaa0?chain_id=56',
       
     },
     {
       key: 3,
-      content: <Item img= {process.env.PUBLIC_URL  + '/nft/products/square.png'} amount= 'PRESALE 10/07' />,
-      link: 'https://www.teaswap.art/nft/products/category/3',
+      content: <Item img={process.env.PUBLIC_URL  + '/nft/products/13hot dog Penguin.png'}  amount= 'PRESALE 11/18'/>,
+      link: 'https://treasureland.market/collection/0x8db099f2d73016cfbdac978bc21e5470e7fbaaa0?chain_id=56',
       
     },
-  
-    // {
-    //   key: 5,
-    //   content: <Item img= {process.env.PUBLIC_URL  + '/nft/products/10_1.gif'} amount='PRESALE 10/12' />,
-    //   link: 'https://www.teaswap.art/nft/products/vendor/264197',
-    // },
     {
       key: 4,
-      content: <Item img= {process.env.PUBLIC_URL  + '/nft/products/penguin_01.gif'} amount= 'PRESALE 10/26'/>,
-      link: 'https://www.teaswap.art/nft/products/vendor/265054',
+      content: <Item img={process.env.PUBLIC_URL  + '/nft/products/14milkshake Penguin.png'}  amount= 'PRESALE 11/18'/>,
+      link: 'https://treasureland.market/collection/0x8db099f2d73016cfbdac978bc21e5470e7fbaaa0?chain_id=56',
       
     },
     {
       key: 5,
-      content: <Item img= {process.env.PUBLIC_URL  + '/nft/products/Penguin Island.png'} amount='PRESALE 11/04' />,
-      link: 'https://www.teaswap.art/nft/products/vendor/265054',
+      content: <Item img={process.env.PUBLIC_URL  + '/nft/products/15hamburger.png'}  amount= 'PRESALE 11/18'/>,
+      link: 'https://treasureland.market/collection/0x8db099f2d73016cfbdac978bc21e5470e7fbaaa0?chain_id=56',
+      
     },
+
+
+
+
+    
+  
+    // {
+    //   key: 2,
+    //   content: <Item img= {process.env.PUBLIC_URL  + '/nft/products/NMsquare.png'} amount= 'PRESALE 10/06' />,
+    //   link: 'https://www.teaswap.art/nft/products/vendor/264197',     
+      
+    // },
+    // {
+    //   key: 3,
+    //   content: <Item img= {process.env.PUBLIC_URL  + '/nft/products/square.png'} amount= 'PRESALE 10/07' />,
+    //   link: 'https://www.teaswap.art/nft/products/category/3',
+      
+    // },
+  
+    // // {
+    // //   key: 5,
+    // //   content: <Item img= {process.env.PUBLIC_URL  + '/nft/products/10_1.gif'} amount='PRESALE 10/12' />,
+    // //   link: 'https://www.teaswap.art/nft/products/vendor/264197',
+    // // },
+    // {
+    //   key: 4,
+    //   content: <Item img= {process.env.PUBLIC_URL  + '/nft/products/penguin_01.gif'} amount= 'PRESALE 10/26'/>,
+    //   link: 'https://www.teaswap.art/nft/products/vendor/265054',
+      
+    // },
+    // {
+    //   key: 5,
+    //   content: <Item img= {process.env.PUBLIC_URL  + '/nft/products/Penguin Island.png'} amount='PRESALE 11/04' />,
+    //   link: 'https://www.teaswap.art/nft/products/vendor/265054',
+    // },
     // {
     //   key: 7,
     //   content: <Item img= {process.env.PUBLIC_URL  + '/nft/products/9.png'} amount='PRESALE 10/26' />,
