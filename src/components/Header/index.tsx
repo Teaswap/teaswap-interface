bc201ca2a17578bad5b936209b6f0bf6b3b7e79c
just like before <agangdi@gmail.com>
2021-11-24 23:35:41 +0800

    header of earn

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -382,6 +382,10 @@ export default function Header() {
             <StyledNavLink id={`swap-nav-link`} to={'/drops'}>
               {t('DROPS')}
             </StyledNavLink>
+            <StyledNavLink id={`NFT-nav-link`} to={'/staking'} >
+              {t('Earn')}
+              {/* <span>↗</span> */}
+            </StyledNavLink>
             <StyledNavLink id={`NFT-nav-link`} to={'/nft/products/category/1'} >
               {t('NFT')}
               {/* <span>↗</span> */}
