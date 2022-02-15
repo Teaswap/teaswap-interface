5d9e0702d287e0aa3ec72045c61d065d6d5ae063
Hooman Safi <hmnsafi@gmail.com>
2022-02-15 11:44:26 -0800

    Link change

--- src/pages/Ido/List.tsx
+++ src/pages/Ido/List.tsx
@@ -45,11 +45,11 @@ const List = () => {
     {
       image: GridImg7,
       Learn: "https://www.teaswap.live/penguinpunks",
-      LearnText: "ETH Mint Learn >",
+      LearnText: "Learn > On ETH ",
       author: "Penguin Punks- The Opportunity for Youth NFT Program",
       medium: "https://www.youtube.com/watch?v=Dfgq197zYTo&t=10s",
       info:
-        "Located in the TSA MetaPlay Park, you will find 12,888 unique TSA Penguins playing around on the Sandbox TSA Meta & TSA MetaPlay Franchise. The NFTs come in different rarity levels: N (Normal ), R (Rare) & SR ( Super Rare).",
+        "A collection of 12,888 unique Penguins (Normal & Rare) curated by TSA incubator and living on the Ethereum, Polygon and Binance Smart Chain. Supporting Youth NFT Programs by giving 5% of proceeds received from public sale to the PenguinPunks NFT Staking Pool for learning adoption in DeFi.",
       joinUs: "penguinpunks",
       when: "When: February 18, 2022",
       end: new Date(1640942516),
@@ -58,13 +58,13 @@ const List = () => {
     },
     {
       image: GridImg7,
-      Learn: "https://www.teaswap.live/penguinpunks",
-      LearnText: "Polygon Mint Learn >",
+      Learn: "https://alpha.niftykit.com/drops/penguinbrother",
+      LearnText: "Learn > On Polygon",
       author: "Penguin Punks- The Opportunity for Youth NFT Program",
       medium: "https://www.youtube.com/watch?v=Dfgq197zYTo&t=10s",
       info:
-        "Located in the TSA MetaPlay Park, you will find 12,888 unique TSA Penguins playing around on the Sandbox TSA Meta & TSA MetaPlay Franchise. The NFTs come in different rarity levels: N (Normal ), R (Rare) & SR ( Super Rare).",
-      joinUs: "penguinpunks",
+        "A collection of 8,888 unique Penguinbrothers (Normal & Rare) curated by TSA Incubator and living on the Ethereum, Polygon and Binance Smart Chain. Supporting Youth NFT Programs by giving 5% of proceeds received from public sale to the PenguinPunks NFT Staking Pool for learning adoption in DeFi.",
+      joinUs: "Alpha",
       when: "When: February 21, 2022",
       end: new Date(1640942516),
       duration: 6048000,
@@ -72,13 +72,13 @@ const List = () => {
     },
     {
       image: GridImg7,
-      Learn: "https://www.teaswap.live/penguinpunks",
-      LearnText: "BSC Mint Learn >",
+      Learn: "https://www.teaswap.art/nft/products/vendor/265609",
+      LearnText: "Learn > On BSC",
       author: "Penguin Punks- The Opportunity for Youth NFT Program",
       medium: "https://www.youtube.com/watch?v=Dfgq197zYTo&t=10s",
       info:
         "Located in the TSA MetaPlay Park, you will find 12,888 unique TSA Penguins playing around on the Sandbox TSA Meta & TSA MetaPlay Franchise. The NFTs come in different rarity levels: N (Normal ), R (Rare) & SR ( Super Rare).",
-      joinUs: "penguinpunks",
+      joinUs: "vendor",
       when: "When: February 23, 2022",
       end: new Date(1640942516),
       duration: 6048000,
@@ -216,11 +216,24 @@ const List = () => {
                     <JoinUs>{t("Join Us")}</JoinUs>
                   </ExternalLink>
                 )}
-                {v.joinUs !== "penguinpunks" && v.joinUs !== "tsapenguinpunks" && (
-                  <StyledLink to={v.joinUs}>
+                {v.joinUs === "Alpha" && (
+                  <ExternalLink href="https://alpha.niftykit.com/drops/penguinbrother">
                     <JoinUs>{t("Join Us")}</JoinUs>
-                  </StyledLink>
+                  </ExternalLink>
                 )}
+                {v.joinUs === "vendor" && (
+                  <ExternalLink href="https://www.teaswap.art/nft/products/vendor/265609">
+                    <JoinUs>{t("Join Us")}</JoinUs>
+                  </ExternalLink>
+                )}
+                {v.joinUs !== "vendor" &&
+                  v.joinUs !== "Alpha" &&
+                  v.joinUs !== "penguinpunks" &&
+                  v.joinUs !== "tsapenguinpunks" && (
+                    <StyledLink to={v.joinUs}>
+                      <JoinUs>{t("Join Us")}</JoinUs>
+                    </StyledLink>
+                  )}
                 {v.joinUs === "tsapenguinpunks" && (
                   <ExternalLink href="https://www.teaswap.live/tsapenguinpunks">
                     <JoinUs>{t("Join Us")}</JoinUs>
